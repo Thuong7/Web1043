97abc781e873e1c7f4217d86a5c512955cafcda6
Thuong7 <hayhayhatt6@gmail.com>
2023-03-21 21:54:11 +0700

    Lab3bai2

--- Lab3/Lab3bai1.js
+++ Lab3/Lab3bai1.js
@@ -1,15 +1,19 @@
 
+//Khai báo và nhập điểm thành phần gồm toán , lý , hóa
 var toan = parseFloat(prompt("Nhập điểm toán: "));
 var ly = parseFloat(prompt("Nhập điểm lý: "));
 var hoa = parseFloat(prompt("Nhập điểm hóa: "));
 var sinh = parseFloat(prompt("Nhập điểm sinh: "));
+//Khai báo học lực và điểm tb
 let hocluc;
 var diemtb = (toan + ly + hoa + sinh )/4;
-
+//Khai báo biến number nhằm phục vụ việc thực thi switch case 
 let number = parseInt(prompt("Menu chương trình\n1. Tính điểm trung bình (switch case)\n2. Tính điểm trung bình (if else)\n0. Thoát \nVui lòng chọn chương trình: "));
+//Thực thi switch case để chạy menu chương trình
 switch(number){
     case 1:
-        document.writeln("Tính điểm trung bình (switch case)");
+        document.writeln("Tính điểm trung bình (switch case)\n");
+        //Thực thi switch case để xét học lực 
         switch(true){
             case diemtb >= 9 && diemtb <= 10:
                 hocluc = "Giỏi";
@@ -25,10 +29,12 @@ switch(number){
                 break;
             
         }
+        // In kết quả ra màn hình
         document.writeln("Điểm trung bình của bạn là: " + diemtb + "  học lực của bạn là: " + hocluc);
         break;
         case 2:
-            document.writeln("Tính điểm trung bình (if else)");
+            // Sử dụng if else để kiểm tra học lực 
+            document.writeln("Tính điểm trung bình (if else)\n");
             if(diemtb >= 9 && diemtb <= 10){
                 hocluc = "Giỏi";
             }else if(diemtb >= 7 && diemtb < 9){
@@ -40,7 +46,7 @@ switch(number){
             }
             document.writeln("Điểm trung bình của bạn là: " + diemtb + "  học lực của bạn là: " + hocluc);
             break;
-            case "0":
+            case 0:
                 window.close();
 }
 
